3834bd9f81eedaa5e692e22602676a36d23ec8b7
Rasmus isager kruuse <isager-kruuse@hotmail.com>
2018-04-17 11:02:19 +0200

    started work on code lock

--- whosthere/whosthere.ino
+++ whosthere/whosthere.ino
@@ -4,14 +4,25 @@
 #define greenLED 7
 #define redLED 6
 #define pizPin A0
-#define KNOCKTHRESHOLD 30
-#define KNOCKPERIOD 75
+#define KNOCKTHRESHOLD 500
+#define RESETTHRESHOLD 5
+#define KNOCKPERIOD 25
 int lastReading = 0;
 int currentReading = 0;
 bool isOpen = false; 
 bool knocked = false;
 bool offCooldown = true;
 unsigned long knockCooldown = 0;
+
+class knockMatchLock{
+  unsigned long key[50];
+  unsigned long input[50];
+  int precision = 10;
+
+  knockMatchLock(int){
+    
+  }
+}
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -23,20 +34,22 @@ void loop() {
   // put your main code here, to run repeatedly:
   currentReading = analogRead(pizPin);
 
-  if(currentReading > KNOCKTHRESHOLD && !knocked && offCooldown){
+  if(currentReading > KNOCKTHRESHOLD  && !knocked && offCooldown){
     isOpen = !isOpen;
     knocked = true;
     Serial.println("lock changed state");
   }
-  if(knocked && currentReading < KNOCKTHRESHOLD && lastReading < KNOCKTHRESHOLD ){
-    Serial.println("cooldown in effect");
+  if(knocked && currentReading < RESETTHRESHOLD && lastReading < RESETTHRESHOLD){
+    Serial.print("cooldown in effect");
     knockCooldown = millis();
     knocked = false;
     offCooldown = false;
+    Serial.println(millis());
   }
-  if(millis() >= (knockCooldown + KNOCKPERIOD)  && offCooldown == false){
+  if(millis() >= (knockCooldown + KNOCKPERIOD) && offCooldown == false){
     offCooldown = true;
-    Serial.println("cooldown in done");
+    Serial.print("cooldown in done");
+    Serial.println(millis());
   }
   
   delay(1);
