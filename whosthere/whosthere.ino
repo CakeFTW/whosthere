b62d222a6de63f01e744a6151cf284df9d33b672
Rasmus isager kruuse <isager-kruuse@hotmail.com>
2018-04-17 13:24:34 +0200

    first sucessfull unlock, there are still some double registration problem, but they might be hardware based. Still need code to actually unlock the locks and deal with that in the state function

--- whosthere/whosthere.ino
+++ whosthere/whosthere.ino
@@ -6,23 +6,147 @@
 #define pizPin A0
 #define KNOCKTHRESHOLD 500
 #define RESETTHRESHOLD 5
-#define KNOCKPERIOD 25
+#define KNOCKPERIOD 100
+#define RECORDDURATION 5000
 int lastReading = 0;
 int currentReading = 0;
-bool isOpen = false; 
 bool knocked = false;
 bool offCooldown = true;
 unsigned long knockCooldown = 0;
+unsigned long timeStamp = 0;
+unsigned long timer; 
 
-class knockMatchLock{
+
+class KnockMatchLock{
+  public:
   unsigned long key[50];
   unsigned long input[50];
-  int precision = 10;
+  int inputCounter = 0;
+  int keyCounter = 0;
+  unsigned long keyStart = 0;
+  unsigned long inputStart = 0;
+  
+  int precision = 50;
+  bool isLocked = false;
+  bool recording = false;
+  bool keyRecorded = false;
+  bool boxClosed = true;
+  
+  KnockMatchLock::KnockMatchLock(int _PRECISION){
+    precision = _PRECISION;
+    for(int i = 0; i < 50; i++){
+      input[i] = 0;
+      key[i] = 0;
+    }
+  };
 
-  knockMatchLock(int){
-    
+  void updateStates(unsigned long stamp){
+    if(recording && !keyRecorded){
+      if(stamp > keyStart + RECORDDURATION){
+        recording = false;
+        keyRecorded = true; 
+        lock();
+      }
+    }
+    if(stamp > inputStart + RECORDDURATION && inputCounter > 0 ){
+      //reset
+      for(int i = 0; i < 50; i++ ){
+        input[i] = 0;
+      }
+      inputCounter = 0;
+    }
+    // check lid status
   }
-}
+
+  void knock(unsigned long &stamp){
+    // if open and lid closed and no key has been recorded start the recording
+    if (boxClosed && !isLocked && !keyRecorded){
+      recording = true;
+    }
+    if(!recording){
+      addIn(stamp);
+    }else{
+      addKey(stamp);
+    }
+  }
+  
+  void addKey(unsigned long &stamp){
+    if(keyCounter == 0){
+      key[0] = 0;
+      keyStart = stamp;
+      keyCounter++;
+    }else{
+      key[keyCounter] = stamp - keyStart;
+      keyCounter++;
+    }
+  }
+
+  void addIn(unsigned long &stamp){
+    if(inputCounter == 0) {
+      inputStart = stamp;
+      inputCounter++;
+    }else{
+      input[inputCounter] = stamp - inputStart;
+      inputCounter++;
+      testMatch();
+    }
+  }
+
+  void printLockData(){
+    Serial.println( " lock states: OPEN: " + String(isLocked) + " RECORDING: " + String(recording) + " KEYRECORDED: " + String(keyRecorded) + " LIDCLOSED: " + String(boxClosed) + "InputStart" + String(inputStart));
+    for(int i = 0 ; i < keyCounter; i++){
+      Serial.print(i);
+      Serial.print( "    " );
+    }
+    Serial.println();
+    for(int i = 0 ; i < keyCounter; i++){
+      Serial.print(key[i]);
+      Serial.print( " " );
+    }
+    Serial.println();
+    for(int i = 0 ; i < keyCounter; i++){
+      Serial.print(input[i]);
+      Serial.print( " " );
+    }
+    Serial.println();
+  }
+
+  bool getLocked(){
+    return isLocked;
+  }
+
+  void lock(){
+    isLocked = true;
+  }
+  void unlock(){
+    isLocked = false;
+  }
+
+  bool testMatch(){
+    if(keyCounter == 0 || inputCounter < keyCounter){
+      return false;
+      }
+    bool matching = true;
+    for(int i = 1; i < keyCounter; i++){
+      if(input[i] < key[i] - precision || input[i] > key[i] + precision){
+        matching = false;
+        Serial.println("FALSE MATCH");
+        Serial.println(String(key[i]) + " vs " + String(input[i]));
+      }
+    }
+    if(matching){
+      unlock();
+      Serial.println("UNLOCKED!!!!!");
+      
+    }else{
+      Serial.println("lock test failed");
+    }
+    return matching;
+  }
+};
+
+KnockMatchLock lock = KnockMatchLock(500);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -33,35 +157,38 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   currentReading = analogRead(pizPin);
+  timeStamp = millis();
+  lock.updateStates(timeStamp);
+  if(timeStamp> timer ){
+    timer = timeStamp + 2000;
+    lock.printLockData();
+  }
 
   if(currentReading > KNOCKTHRESHOLD  && !knocked && offCooldown){
-    isOpen = !isOpen;
     knocked = true;
-    Serial.println("lock changed state");
+    lock.knock(timeStamp);
+    //Serial.println("lock changed state");
   }
   if(knocked && currentReading < RESETTHRESHOLD && lastReading < RESETTHRESHOLD){
-    Serial.print("cooldown in effect");
-    knockCooldown = millis();
+    //Serial.print("cooldown in effect");
+    knockCooldown = timeStamp;
     knocked = false;
     offCooldown = false;
-    Serial.println(millis());
+    //Serial.println(millis());
   }
-  if(millis() >= (knockCooldown + KNOCKPERIOD) && offCooldown == false){
+  if(timeStamp >= (knockCooldown + KNOCKPERIOD) && offCooldown == false){
     offCooldown = true;
-    Serial.print("cooldown in done");
-    Serial.println(millis());
+    //Serial.print("cooldown in done");
+    //Serial.println(millis());
   }
   
-  delay(1);
-  if(isOpen){
-    digitalWrite(greenLED, HIGH);
-    digitalWrite(redLED, LOW);
+  bool lockState = lock.getLocked();
+  if(lockState){
+    digitalWrite(greenLED, LOW);
+    digitalWrite(redLED, HIGH);
   }else{
     digitalWrite(greenLED, LOW);
     digitalWrite(redLED,HIGH);
   }
   lastReading = currentReading;
-  if(currentReading > 5){
-    Serial.println(currentReading);
-  }
 }
